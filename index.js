29ea9d92ef0c6114cd71a2423aab784540a41d86
Sheropana <giovanalipes941@gmail.com>
2024-08-27 10:13:43 -0300

    first commit

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ import { montadoras, veiculos} from './dados.js';
 
 const app = express();
 
+app.get('/', (req,res)=>{
+    res.json({"status": "Server sOK"});
+})
+
 app.get('/veiculo', (req,res)=>{
-    
+    res.json(veiculos);
+})
+
+app.get('/montadoras', (req,res)=>{
+    res.json(montadoras);
+})
+
+const PORT = 3030;
+app.listen(PORT,()=>{
+    console.log('Sistema inicializado: ', `Acesso: http://localhost:$(PORT)`)
 })
